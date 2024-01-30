bb25b848773d50f8c3d643d8651b5cfcb18a5ab1
marioaje <marioaje@gmail.com>
2024-01-29 19:01:29 -0600

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 
@@ -10,25 +9,35 @@ int factorial(int n) {
 	if (n < 0) {
 		printf("El factorial no es valido ");
 		return -1;
-	}
-	
-	
+	}	
+	//iterativo
 	for (int i = 2; i <= n; i++) {
 		resultado *= i;
 	}
 
-	return resultado;
-
-	
+	return resultado;	
 }
+
+//factorial version recursiva
+int factorialrecursiva(int n) {
+	if (n == 0 || n == 1)
+		return 1;
+	else
+		return n * factorialrecursiva(n - 1);
+}
+
+
 
 int main() {
 	int n = 8;
 
 	int resultado = factorial(8);
 	if (resultado != -1) {
-		printf("El factorial de:", n);
-		printf("Es:", resultado);
+		printf("El factorial de %d:", n);
+		printf("Es %d:", resultado);
 	}
 
+
+	int resultadofactorial = factorialrecursiva(8);
+	printf("Es %d:", resultadofactorial);
 }
